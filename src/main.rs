61d27b02e26660d53baa6116d3331c4fedaf805d
hongcha <zhangsifan98@gmail.com>
2023-08-14 22:30:33 +0800

    impl whip whep protocol

--- src/main.rs
+++ src/main.rs
@@ -1,227 +1,70 @@
-use anyhow::Result;
-use axum::{
-    extract::{Request, State},
-    http::StatusCode,
-    response::IntoResponse,
-    routing::post,
-    Router,
-};
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Router, routing::post};
 use hyper::header;
-use std::sync::{Arc, Mutex, RwLock};
-use tokio::net::UdpSocket;
-use tokio::sync::mpsc::{channel, Sender};
 use tower_http::services::{ServeDir, ServeFile};
-use webrtc::{
-    api::interceptor_registry::register_default_interceptors,
-    api::media_engine::{MediaEngine, MIME_TYPE_VP8},
-    api::APIBuilder,
-    ice_transport::ice_connection_state::RTCIceConnectionState,
-    ice_transport::ice_server::RTCIceServer,
-    interceptor::registry::Registry,
-    peer_connection::configuration::RTCConfiguration,
-    peer_connection::peer_connection_state::RTCPeerConnectionState,
-    peer_connection::sdp::session_description::RTCSessionDescription,
-    rtp_transceiver::rtp_codec::RTCRtpCodecCapability,
-    track::track_local::track_local_static_rtp::TrackLocalStaticRTP,
-    track::track_local::{TrackLocal, TrackLocalWriter},
-    Error,
-};
+use webrtc::peer_connection::sdp::session_description::RTCSessionDescription;
 
-async fn start_webrtc(offer: RTCSessionDescription) -> (RTCSessionDescription, Sender<Vec<u8>>) {
-    // Create a MediaEngine object to configure the supported codec
-    let mut m = MediaEngine::default();
+use crate::rtc::PeerForward;
 
-    m.register_default_codecs().unwrap();
+mod rtc;
 
-    // Create a InterceptorRegistry. This is the user configurable RTP/RTCP Pipeline.
-    // This provides NACKs, RTCP Reports and other features. If you use `webrtc.NewPeerConnection`
-    // this is enabled by default. If you are manually managing You MUST create a InterceptorRegistry
-    // for each PeerConnection.
-    let mut registry = Registry::new();
-
-    // Use the default set of Interceptors
-    registry = register_default_interceptors(registry, &mut m).unwrap();
-
-    // Create the API object with the MediaEngine
-    let api = APIBuilder::new()
-        .with_media_engine(m)
-        .with_interceptor_registry(registry)
-        .build();
-
-    // Prepare the configuration
-    let config = RTCConfiguration {
-        ice_servers: vec![RTCIceServer {
-            urls: vec!["stun:stun.l.google.com:19302".to_owned()],
-            ..Default::default()
-        }],
-        ..Default::default()
-    };
-
-    // Create a new RTCPeerConnection
-    let peer_connection = Arc::new(api.new_peer_connection(config).await.unwrap());
-
-    // Create Track that we send video back to browser on
-    let video_track = Arc::new(TrackLocalStaticRTP::new(
-        RTCRtpCodecCapability {
-            mime_type: MIME_TYPE_VP8.to_owned(),
-            ..Default::default()
-        },
-        "video".to_owned(),
-        "webrtc-rs".to_owned(),
-    ));
-
-    // Add this newly created track to the PeerConnection
-    let rtp_sender = peer_connection
-        .add_track(Arc::clone(&video_track) as Arc<dyn TrackLocal + Send + Sync>)
-        .await
-        .unwrap();
-
-    // Read incoming RTCP packets
-    // Before these packets are returned they are processed by interceptors. For things
-    // like NACK this needs to be called.
-    tokio::spawn(async move {
-        let mut rtcp_buf = vec![0u8; 1500];
-        while let Ok((_, _)) = rtp_sender.read(&mut rtcp_buf).await {}
-        Result::<()>::Ok(())
-    });
-
-    //let (done_tx, mut done_rx) = tokio::sync::mpsc::channel::<()>(1);
-
-    //let done_tx1 = done_tx.clone();
-    // Set the handler for ICE connection state
-    // This will notify you when the peer has connected/disconnected
-    peer_connection.on_ice_connection_state_change(Box::new(
-        move |connection_state: RTCIceConnectionState| {
-            println!("Connection State has changed {connection_state}");
-            //if connection_state == RTCIceConnectionState::Failed {
-            //    let _ = done_tx1.try_send(());
-            //}
-            Box::pin(async {})
-        },
-    ));
-
-    //let done_tx2 = done_tx.clone();
-    // Set the handler for Peer connection state
-    // This will notify you when the peer has connected/disconnected
-    peer_connection.on_peer_connection_state_change(Box::new(move |s: RTCPeerConnectionState| {
-        println!("Peer Connection State has changed: {s}");
-
-        if s == RTCPeerConnectionState::Failed {
-            // Wait until PeerConnection has had no network activity for 30 seconds or another failure. It may be reconnected using an ICE Restart.
-            // Use webrtc.PeerConnectionStateDisconnected if you are interested in detecting faster timeout.
-            // Note that the PeerConnection may come back from PeerConnectionStateDisconnected.
-            println!("Peer Connection has gone to failed exiting: Done forwarding");
-            //let _ = done_tx2.try_send(());
-        }
-
-        Box::pin(async {})
-    }));
-
-    // Set the remote SessionDescription
-    peer_connection.set_remote_description(offer).await.unwrap();
-
-    // Create an answer
-    let answer = peer_connection.create_answer(None).await.unwrap();
-
-    // Create channel that is blocked until ICE Gathering is complete
-    let mut gather_complete = peer_connection.gathering_complete_promise().await;
-
-    // Sets the LocalDescription, and starts our UDP listeners
-    peer_connection.set_local_description(answer).await.unwrap();
-
-    // Block until ICE Gathering is complete, disabling trickle ICE
-    // we do this because we only can exchange one signaling message
-    // in a production application you should exchange ICE Candidates via OnICECandidate
-    let _ = gather_complete.recv().await;
-
-    let (tx, mut rx) = channel::<Vec<u8>>(32);
-
-    //let done_tx3 = done_tx.clone();
-    tokio::spawn(async move {
-        while let Some(msg) = rx.recv().await {
-            if let Err(err) = video_track.write(&msg).await {
-                if Error::ErrClosedPipe == err {
-                    // The peerConnection has been closed.
-                } else {
-                    println!("video_track write err: {err}");
-                }
-                //let _ = done_tx3.try_send(());
-                return;
-            }
-        }
-    });
-
-    //println!("Press ctrl-c to stop");
-    //tokio::select! {
-    //    _ = done_rx.recv() => {
-    //        println!("received done signal!");
-    //    }
-    //    _ = tokio::signal::ctrl_c() => {
-    //        println!();
-    //    }
-    //};
-
-    // TODO:
-    // peer_connection.close().await?;
-
-    (peer_connection.local_description().await.unwrap(), tx)
-}
 
 #[tokio::main]
 async fn main() {
     let shared_state = SharedState::default();
-    let state = Arc::clone(&shared_state);
-
-    tokio::spawn(async move {
-        rtp_listener(state).await;
-    });
-
     let serve_dir = ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
-
-    // build our application with a route
     let app = Router::new()
-        .route("/whep/endpoint", post(webrtc_handler))
+        .route("/whip", post(whip))
+        .route("/whep/endpoint", post(whep))
         .nest_service("/", serve_dir.clone())
         .with_state(Arc::clone(&shared_state));
-
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-//ToString -> RTCSessionDescription
-async fn webrtc_handler(State(state): State<SharedState>, request: Request) -> impl IntoResponse {
-    let body = request.into_body();
-    let body_bytes = hyper::body::to_bytes(body).await.unwrap();
-    let body_string = String::from_utf8_lossy(&body_bytes).to_string();
-    //new RTCSessionDescription
-    let whep_offer = RTCSessionDescription::offer(body_string).unwrap();
-    let (answer, sender) = start_webrtc(whep_offer).await;
-    state.write().unwrap().ch.lock().unwrap().push(sender);
-    (
-        StatusCode::CREATED,
-        [(header::CONTENT_TYPE, "application/sdp")],
-        //[(header::LOCATION, url+"whep/endpoint")],
-        answer.sdp,
-    )
+type SharedState = Arc<PeerForward>;
+
+async fn whip(State(state): State<SharedState>, body: String) -> impl IntoResponse {
+    let offer = RTCSessionDescription::offer(body).unwrap();
+    match state.set_anchor(offer).await {
+        Ok(sdp) => {
+            (
+                StatusCode::CREATED,
+                [(header::CONTENT_TYPE, "application/sdp")],
+                sdp.sdp,
+            )
+        }
+        Err(err) => {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                [(header::CONTENT_TYPE, "application/sdp")],
+                err.to_string(),
+            )
+        }
+    }
 }
 
-type SharedState = Arc<RwLock<AppState>>;
-
-#[derive(Default)]
-struct AppState {
-    ch: Mutex<Vec<Sender<Vec<u8>>>>,
-} //监听rtp
-async fn rtp_listener(state: SharedState) {
-    let listener = UdpSocket::bind("127.0.0.1:5004").await.unwrap();
-    println!("=== RTP listener started ===");
-
-    let mut inbound_rtp_packet = vec![0u8; 1600]; // UDP MTU
-    while let Ok((n, _)) = listener.recv_from(&mut inbound_rtp_packet).await {
-        let data = inbound_rtp_packet[..n].to_vec();
-        let arr = state.read().unwrap().ch.lock().unwrap().clone();
-        for c in arr.iter() {
-            c.send(data.clone()).await.unwrap();
+async fn whep(State(state): State<SharedState>, body: String) -> impl IntoResponse {
+    let offer = RTCSessionDescription::offer(body).unwrap();
+    match state.add_subscribe(offer).await {
+        Ok(sdp) => {
+            (
+                StatusCode::CREATED,
+                [(header::CONTENT_TYPE, "application/sdp")],
+                sdp.sdp,
+            )
+        }
+        Err(err) => {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                [(header::CONTENT_TYPE, "application/sdp")],
+                err.to_string(),
+            )
         }
     }
 }
---
+++ src/rtc.rs
@@ -0,0 +1,390 @@
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
+use std::sync::Arc;
+use std::time::Duration;
+
+use anyhow::Result;
+use tokio::sync::mpsc::{channel, Sender};
+use tokio::sync::{RwLock, RwLockReadGuard};
+use webrtc::api::APIBuilder;
+use webrtc::api::interceptor_registry::register_default_interceptors;
+use webrtc::api::media_engine::{MediaEngine, MIME_TYPE_OPUS, MIME_TYPE_VP8};
+use webrtc::ice_transport::ice_connection_state::RTCIceConnectionState;
+use webrtc::ice_transport::ice_server::RTCIceServer;
+use webrtc::interceptor::registry::Registry;
+use webrtc::peer_connection::configuration::RTCConfiguration;
+use webrtc::peer_connection::peer_connection_state::RTCPeerConnectionState;
+use webrtc::peer_connection::RTCPeerConnection;
+use webrtc::peer_connection::sdp::session_description::RTCSessionDescription;
+use webrtc::rtcp::payload_feedbacks::picture_loss_indication::PictureLossIndication;
+use webrtc::rtp::packet::Packet;
+use webrtc::rtp_transceiver::PayloadType;
+use webrtc::rtp_transceiver::rtp_codec::{
+    RTCRtpCodecCapability, RTCRtpCodecParameters, RTPCodecType,
+};
+use webrtc::track::track_local::{TrackLocal, TrackLocalWriter};
+use webrtc::track::track_local::track_local_static_rtp::TrackLocalStaticRTP;
+use webrtc::track::track_remote::TrackRemote;
+
+pub const VIDEO_KIND: &str = "video";
+pub const AUDIO_KIND: &str = "audio";
+pub const VIDEO_PAYLOAD_TYPE: PayloadType = 96;
+pub const AUDIO_PAYLOAD_TYPE: PayloadType = 111;
+
+pub type ForwardData = Arc<Packet>;
+
+pub struct Peer(pub Arc<RTCPeerConnection>);
+
+pub struct Anchor(Arc<RTCPeerConnection>, bool, bool);
+
+pub struct Subscribe(Option<Sender<ForwardData>>, Option<Sender<ForwardData>>);
+
+#[derive(Default)]
+pub struct Subscription(Option<Sender<ForwardData>>, Option<Sender<ForwardData>>);
+
+impl From<Arc<RTCPeerConnection>> for Peer {
+    fn from(value: Arc<RTCPeerConnection>) -> Self {
+        Peer(value)
+    }
+}
+
+impl Clone for Peer {
+    fn clone(&self) -> Self {
+        Peer(self.0.clone())
+    }
+}
+
+impl Eq for Peer {}
+
+impl PartialEq for Peer {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.get_stats_id() == other.0.get_stats_id()
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        self.0.get_stats_id() != other.0.get_stats_id()
+    }
+}
+
+impl Hash for Peer {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.get_stats_id().hash(state);
+    }
+}
+
+#[derive(Default)]
+pub struct PeerForward {
+    anchor: Arc<RwLock<Option<Anchor>>>,
+    subscription_map: Arc<RwLock<HashMap<Peer, Subscription>>>,
+}
+
+impl Clone for PeerForward {
+    fn clone(&self) -> Self {
+        PeerForward {
+            anchor: self.anchor.clone(),
+            subscription_map: self.subscription_map.clone(),
+        }
+    }
+}
+
+impl PeerForward {
+    pub async fn set_anchor(&self, offer: RTCSessionDescription) -> Result<RTCSessionDescription> {
+        let mut anchor = self.anchor.write().await;
+        if anchor.is_some() {
+            return Err(anyhow::anyhow!("anchor is set"));
+        }
+        let peer = PeerForward::new_peer().await?;
+        peer.on_ice_connection_state_change(Box::new(
+            move |connection_state: RTCIceConnectionState| {
+                println!("Connection State has changed {connection_state}");
+                Box::pin(async {})
+            },
+        ));
+        let self_arc = Arc::new(self.clone());
+        let pc = peer.clone();
+        peer.on_peer_connection_state_change(Box::new(move |s: RTCPeerConnectionState| {
+            let self_arc = self_arc.clone();
+            let pc = pc.clone();
+            tokio::spawn(async move {
+                println!("Peer Connection State has changed: {s}");
+                if s == RTCPeerConnectionState::Failed
+                    || s == RTCPeerConnectionState::Closed
+                    || s == RTCPeerConnectionState::Disconnected
+                {
+                    let _ = pc.close().await;
+                    let anchor = self_arc.anchor.clone();
+                    let mut anchor = anchor.write().await;
+                    *anchor = None;
+                };
+            });
+            Box::pin(async {})
+        }));
+        let self_arc = Arc::new(self.clone());
+        let pc = peer.clone();
+        peer.on_track(Box::new(move |track, _, _| {
+            let self_arc = self_arc.clone();
+            let pc2 = pc.clone();
+            tokio::spawn(async move {
+                let _ = self_arc.anchor_up_track(pc2, track).await;
+            });
+            Box::pin(async {})
+        }));
+        let _ = peer.set_remote_description(offer).await?;
+        let answer = peer.create_answer(None).await?;
+        let mut gather_complete = peer.gathering_complete_promise().await;
+        let _ = peer.set_local_description(answer).await?;
+        let _ = gather_complete.recv().await;
+        let description = peer
+            .local_description()
+            .await
+            .ok_or(anyhow::anyhow!("Failed to get local description"))?;
+        *anchor = Some(Anchor(peer, false, false));
+        Ok(description)
+    }
+
+    pub async fn add_subscribe(
+        &self,
+        offer: RTCSessionDescription,
+    ) -> Result<RTCSessionDescription> {
+        let peer = PeerForward::new_peer().await?;
+        peer.on_ice_connection_state_change(Box::new(
+            move |connection_state: RTCIceConnectionState| {
+                println!("Connection State has changed {connection_state}");
+                Box::pin(async {})
+            },
+        ));
+        let pc = peer.clone();
+        let self_arc = Arc::new(self.clone());
+        peer.on_peer_connection_state_change(Box::new(move |s: RTCPeerConnectionState| {
+            let pc = pc.clone();
+            let self_arc = self_arc.clone();
+            let subscription_map = self_arc.subscription_map.clone();
+            Box::pin(async move {
+                println!("Peer Connection State has changed: {s}");
+                match s {
+                    RTCPeerConnectionState::Failed | RTCPeerConnectionState::Closed | RTCPeerConnectionState::Disconnected => {
+                        let _ = pc.close().await;
+                        let mut subscribe_peers = subscription_map.write().await;
+                        subscribe_peers.remove(&pc.into());
+                    }
+                    RTCPeerConnectionState::Connected => {
+                        let mut subscribe_peers = subscription_map.write().await;
+                        subscribe_peers.insert(pc.into(), Subscription(None, None));
+                        drop(subscribe_peers);
+                        let _ = self_arc.subscribe_refresh(VIDEO_KIND).await;
+                        let _ = self_arc.subscribe_refresh(AUDIO_KIND).await;
+                    }
+                    _ => {}
+                };
+            })
+        }));
+        let _ = peer.set_remote_description(offer).await?;
+        let answer = peer.create_answer(None).await?;
+        let mut gather_complete = peer.gathering_complete_promise().await;
+        let _ = peer.set_local_description(answer).await?;
+        let _ = gather_complete.recv().await;
+        let description = peer
+            .local_description()
+            .await
+            .ok_or(anyhow::anyhow!("Failed to get local description"))?;
+        Ok(description)
+    }
+
+    async fn anchor_up_track(
+        &self,
+        peer: Arc<RTCPeerConnection>,
+        track: Arc<TrackRemote>,
+    ) -> Result<()> {
+        let kind = track.kind().to_string();
+        println!("anchor_up_track : {kind}");
+        tokio::spawn(PeerForward::publish_track_remote_pli(
+            peer.clone(),
+            track.clone(),
+        ));
+        let self_arc = Arc::new(self.clone());
+        tokio::spawn(async move {
+            self_arc.publish_track_remote(track.clone()).await;
+        });
+        let _ = self.subscribe_refresh(kind.as_str()).await;
+        Ok(())
+    }
+
+    async fn subscribe_refresh(&self, kind: &str) -> Result<()> {
+        let anchor = self.anchor.read().await;
+        if anchor.is_none() {
+            return Ok(());
+        }
+        drop(anchor);
+        let mut peers = self.subscription_map.write().await;
+        let subscribe_refresh_peers: Vec<Peer> = peers
+            .iter()
+            .filter(|(_, subscription)| {
+                match kind {
+                    VIDEO_KIND => subscription.0.is_none(),
+                    AUDIO_KIND => subscription.1.is_none(),
+                    _ => false,
+                }
+            })
+            .map(|(p, _)| p.clone())
+            .collect();
+        for peer in subscribe_refresh_peers {
+            match PeerForward::peer_add_track(peer.0.clone(), kind).await {
+                Ok(sender) => {
+                    let mut subscription = peers.get_mut(&peer).unwrap();
+                    match kind {
+                        VIDEO_KIND => subscription.0 = Some(sender),
+                        AUDIO_KIND => subscription.1 = Some(sender),
+                        _ => {}
+                    }
+                }
+                Err(err) => {
+                    println!("peer_add_track err: {} peer id : {}", err, peer.0.get_stats_id());
+                }
+            }
+        }
+        Ok(())
+    }
+
+
+    async fn publish_track_remote(&self, track: Arc<TrackRemote>) {
+        let kind = track.kind().to_string();
+        let kind = kind.as_str();
+        let mut b = vec![0u8; 1500];
+        while let Ok((rtp_packet, _)) = track.read(&mut b).await {
+            let subscription_map = self.subscription_map.read().await;
+            let senders: Vec<Sender<ForwardData>> = subscription_map
+                .iter()
+                .map(|(_, subscription)| {
+                    match kind {
+                        VIDEO_KIND => subscription.0.clone(),
+                        AUDIO_KIND => subscription.1.clone(),
+                        _ => None,
+                    }
+                })
+                .filter(|subscription| subscription.is_some())
+                .map(|subscription| subscription.unwrap())
+                .collect();
+            drop(subscription_map);
+            let packet = Arc::new(rtp_packet);
+            for sender in senders.iter() {
+                let _ = sender.send(packet.clone()).await;
+            }
+        }
+    }
+
+    async fn publish_track_remote_pli(peer: Arc<RTCPeerConnection>, track: Arc<TrackRemote>) {
+        let pc = Arc::downgrade(&peer);
+        // Send a PLI on an interval so that the publisher is pushing a keyframe every rtcpPLIInterval
+        let media_ssrc = track.ssrc();
+        let pc2 = pc.clone();
+        tokio::spawn(async move {
+            let mut result = Result::<usize>::Ok(0);
+            while result.is_ok() {
+                let timeout = tokio::time::sleep(Duration::from_secs(1));
+                tokio::pin!(timeout);
+                tokio::select! {
+                    _ = timeout.as_mut() =>{
+                        if let Some(pc) = pc2.upgrade(){
+                            result = pc.write_rtcp(&[Box::new(PictureLossIndication{
+                                sender_ssrc: 0,
+                                media_ssrc,
+                            })]).await.map_err(Into::into);
+                        }else{
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    async fn new_peer() -> Result<Arc<RTCPeerConnection>> {
+        let mut m = MediaEngine::default();
+        m.register_codec(
+            RTCRtpCodecParameters {
+                capability: RTCRtpCodecCapability {
+                    mime_type: MIME_TYPE_VP8.to_owned(),
+                    clock_rate: 90000,
+                    channels: 0,
+                    sdp_fmtp_line: "".to_owned(),
+                    rtcp_feedback: vec![],
+                },
+                payload_type: VIDEO_PAYLOAD_TYPE,
+                ..Default::default()
+            },
+            RTPCodecType::Video,
+        )?;
+
+        m.register_codec(
+            RTCRtpCodecParameters {
+                capability: RTCRtpCodecCapability {
+                    mime_type: MIME_TYPE_OPUS.to_owned(),
+                    clock_rate: 48000,
+                    channels: 2,
+                    sdp_fmtp_line: "".to_owned(),
+                    rtcp_feedback: vec![],
+                },
+                payload_type: AUDIO_PAYLOAD_TYPE,
+                ..Default::default()
+            },
+            RTPCodecType::Audio,
+        )?;
+
+        let mut registry = Registry::new();
+
+        registry = register_default_interceptors(registry, &mut m)?;
+
+        let api = APIBuilder::new()
+            .with_media_engine(m)
+            .with_interceptor_registry(registry)
+            .build();
+
+        let config = RTCConfiguration {
+            ice_servers: vec![RTCIceServer {
+                urls: vec!["stun:stun.l.google.com:19302".to_owned()],
+                ..Default::default()
+            }],
+            ..Default::default()
+        };
+        return Ok(Arc::new(api.new_peer_connection(config).await?));
+    }
+
+    async fn peer_add_track(
+        peer: Arc<RTCPeerConnection>,
+        kind: &str,
+    ) -> Result<Sender<ForwardData>> {
+        let (mime_type, id, stream_id) = match kind {
+            VIDEO_KIND => (
+                MIME_TYPE_VP8.to_owned(),
+                VIDEO_KIND.to_owned(),
+                "webrtc-rs-video".to_owned(),
+            ),
+            AUDIO_KIND => (
+                MIME_TYPE_OPUS.to_owned(),
+                AUDIO_KIND.to_owned(),
+                "webrtc-rs-audio".to_owned(),
+            ),
+            _ => return Err(anyhow::anyhow!("kind error")),
+        };
+        let track = Arc::new(TrackLocalStaticRTP::new(
+            RTCRtpCodecCapability {
+                mime_type,
+                ..Default::default()
+            },
+            id,
+            stream_id,
+        ));
+        let _ = peer
+            .add_track(Arc::clone(&track) as Arc<dyn TrackLocal + Send + Sync>)
+            .await?;
+        let (send, mut recv) = channel::<ForwardData>(32);
+        tokio::spawn(async move {
+            while let Some(data) = recv.recv().await {
+                if let Err(err) = track.write_rtp(&data).await {
+                    println!("video_track.write err: {}", err);
+                }
+            }
+        });
+        Ok(send)
+    }
+}
