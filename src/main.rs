9a38f2b08157298885a807cb0d73635dc4ec22f2
hongcha <zhangsifan98@gmail.com>
2023-08-27 22:57:47 +0800

    update link_header

--- src/main.rs
+++ src/main.rs
@@ -196,18 +196,29 @@ fn link_header(ice_servers: Vec<IceServer>) -> Vec<String> {
     ice_servers
         .into_iter()
         .flat_map(|server| {
+            let mut username = server.username;
+            let mut credential = server.credential;
+            if username != "" {
+                username = string_encoder(&username);
+                credential = string_encoder(&credential);
+            }
             server.urls.into_iter().map(move |url| {
-                let mut link = format!(">{}; rel=\"ice-server\"", url);
-                if server.username != "" {
+                let mut link = format!("<{}>; rel=\"ice-server\"", url);
+                if username != "" {
                     link = format!(
                         "{}; username=\"{}\"; credential=\"{}\"; credential-type=\"{}\"",
-                        link, server.username, server.credential, server.credential_type
+                        link, username, credential, server.credential_type
                     );
                 }
                 link
             })
         })
         .collect()
+}
+
+fn string_encoder(s: &impl ToString) -> String {
+    let s = serde_json::to_string(&s.to_string()).unwrap();
+    s[1..s.len() - 1].to_string()
 }
 
 struct AppError(anyhow::Error);
