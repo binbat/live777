d201449e38382fa121a92026147018a240f9ba34
a-wing <1@233.email>
2024-03-30 22:43:08 +0800

    fix webui switch dark mode text color error

--- web/app.tsx
+++ web/app.tsx
@@ -38,7 +38,7 @@ export function App() {
             <label class="inline-flex items-center cursor-pointer">
                 <input type="checkbox" value="" class="sr-only peer" checked={!!refTimer.current} onClick={triggerTimer} />
                 <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Auto Refresh</span>
+                <span class="ms-3 text-sm font-medium dark:text-gray-300">Auto Refresh</span>
             </label>
 
             <table>
