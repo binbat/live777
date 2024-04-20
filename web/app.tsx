e49ede265761765e4b282ee6db79a5f41f32e339
Rocka <i@rocka.me>
2024-04-21 01:49:41 +0800

    extract dialog components

--- web/app.tsx
+++ web/app.tsx
@@ -1,40 +1,26 @@
 import { useState, useRef, useEffect } from 'preact/hooks'
 import Logo from '/logo.svg'
 import './app.css'
-import { Dialog } from './dialog'
-import {
-    allStream,
-    delStream,
-    reforward,
-} from './api'
-
-const formatTime = (timestamp: number) => new Date(timestamp).toLocaleString('zh-CN', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hourCycle: 'h23'
-});
+import { allStream, delStream } from './api'
+import { formatTime } from './utils'
+import { IClientsDialog, ClientsDialog } from './dialog-clients'
+import { IReforwardDialog, ReforwardDialog } from './dialog-reforward'
 
 export function App() {
-    const [streamId, setStreamId] = useState<string>("")
-    const [items, setItems] = useState<any[]>([])
-    const [pubItems, setPubItems] = useState<any[]>([])
+    const [streams, setStreams] = useState<any[]>([])
+    const [selectedStreamId, setSelectedStreamId] = useState('')
     const refTimer = useRef<null | ReturnType<typeof setInterval>>(null)
-    const refDialog = useRef<HTMLDialogElement>(null)
-    const refConfirm = useRef<HTMLButtonElement>(null)
-    const refInput = useRef<HTMLInputElement>(null)
+    const refReforward = useRef<IReforwardDialog>(null)
+    const refViewClients = useRef<IClientsDialog>(null)
 
     const updateAllStreams = async () => {
-        setItems(await allStream())
+        setStreams(await allStream())
     }
 
     // fetch all streams on component mount
     useEffect(() => { updateAllStreams() }, [])
 
-    const triggerTimer = () => {
+    const toggleTimer = () => {
         if (refTimer.current) {
             clearInterval(refTimer.current)
             refTimer.current = null
@@ -44,17 +30,13 @@ export function App() {
         }
     }
 
-    const triggerForward = (streamId: string) => {
-        refDialog.current?.showModal()
+    const handleViewClients = (id: string) => {
+        setSelectedStreamId(id)
+        refViewClients.current?.show()
+    }
 
-        if (refInput.current) refInput.current.value = ""
-
-        if (refDialog.current) refDialog.current.onclose = () => {
-            //targetUrl: "http://localhost:7777/whip/888",
-            const target = refDialog.current?.returnValue
-            console.log(target)
-            if (target) reforward(streamId, target)
-        }
+    const handleReforwardStream = (id: string) => {
+        refReforward.current?.show(id)
     }
 
     return (
@@ -65,33 +47,15 @@ export function App() {
                 </a>
             </div>
 
-            <Dialog streamId={streamId} items={pubItems} />
+            <ClientsDialog ref={refViewClients} id={selectedStreamId} clients={streams.find(s => s.id == selectedStreamId)?.subscribeSessionInfos ?? []} />
 
-            <dialog ref={refDialog}>
-                <form method="dialog">
-                    <p>
-                        <label
-                        >Target Url:
-                            <input ref={refInput} type="text" onChange={e => {
-                                if (refConfirm.current && e.target) {
-                                    //@ts-ignore
-                                    refConfirm.current.value = e.target.value
-                                }
-                            }} />
-                        </label>
-                    </p>
-                    <div>
-                        <button value="">Cancel</button>
-                        <button ref={refConfirm} value="">Confirm</button>
-                    </div>
-                </form>
-            </dialog>
+            <ReforwardDialog ref={refReforward} />
 
             <fieldset>
                 <legend class="inline-flex items-center">
-                    <span>Streams (total: {items.length})</span>
+                    <span>Streams (total: {streams.length})</span>
                     <label class="ml-10 inline-flex items-center cursor-pointer">
-                        <input type="checkbox" value="" class="sr-only peer" checked={!!refTimer.current} onClick={triggerTimer} />
+                        <input type="checkbox" value="" class="sr-only peer" checked={!!refTimer.current} onClick={toggleTimer} />
                         <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                         <span class="ml-2">Auto Refresh</span>
                     </label>
@@ -101,7 +65,7 @@ export function App() {
                 <table>
                     <thead>
                         <tr>
-                            <th class="mw-50 text-center">ID</th>
+                            <th class="mw-50">ID</th>
                             <th>Publisher</th>
                             <th>Subscriber</th>
                             <th>Reforward</th>
@@ -110,7 +74,7 @@ export function App() {
                         </tr>
                     </thead>
                     <tbody>
-                        {items.map(i =>
+                        {streams.map(i =>
                             <tr>
                                 <td class="text-center">{i.id}</td>
                                 <td class="text-center">{i.publishLeaveTime === 0 ? "Ok" : "No"}</td>
@@ -119,11 +83,8 @@ export function App() {
                                 <td class="text-center">{formatTime(i.createTime)}</td>
                                 <td>
                                     <button onClick={() => delStream(i.id, i.publishSessionInfo.id)}>Destroy</button>
-                                    <button onClick={() => {
-                                        setStreamId(i.id)
-                                        setPubItems(i.subscribeSessionInfos)
-                                    }}>Kick</button>
-                                    <button onClick={() => triggerForward(i.id)}>Reforward</button>
+                                    <button onClick={() => handleViewClients(i.id)}>Clients</button>
+                                    <button onClick={() => handleReforwardStream(i.id)}>Reforward</button>
                                 </td>
                             </tr>
                         )}
---
+++ web/dialog-clients.tsx
@@ -0,0 +1,53 @@
+import { useRef, useImperativeHandle } from 'preact/hooks'
+import { forwardRef } from 'preact/compat'
+import { delStream } from './api'
+import { formatTime } from './utils'
+
+interface Props {
+    id: string
+    clients: any[]
+}
+
+export interface IClientsDialog {
+    show(): void
+}
+
+export const ClientsDialog = forwardRef<IClientsDialog, Props>((props: Props, ref) => {
+    const refDialog = useRef<HTMLDialogElement>(null)
+
+    useImperativeHandle(ref, () => {
+        return {
+            show: () => {
+                refDialog.current?.showModal()
+            }
+        }
+    })
+
+    return (
+        <dialog ref={refDialog}>
+            <h3>Clients</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>State</th>
+                        <th>Creation Time</th>
+                        <th>Operation</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.clients.map(c =>
+                        <tr>
+                            <td>{c.id} {c.reforward ? "(reforward)" : ""}</td>
+                            <td>{c.connectState}</td>
+                            <td>{formatTime(c.createTime)}</td>
+                            <td><button onClick={() => delStream(props.id, c.id)}>Kick</button></td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+            <br />
+            <button onClick={() => refDialog.current?.close()}>Close</button>
+        </dialog>
+    )
+})
