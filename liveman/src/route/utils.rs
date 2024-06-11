2eae02cfc33dce75aa893a04c78ef51df99605ef
a-wing <1@233.email>
2024-06-11 15:54:39 +0800

    remove liveman check peer connection state

--- liveman/src/route/utils.rs
+++ liveman/src/route/utils.rs
@@ -1,5 +1,5 @@
 use anyhow::{anyhow, Error};
-use live777_http::{request::Reforward, response::RTCPeerConnectionState};
+use live777_http::request::Reforward;
 use reqwest::header::HeaderMap;
 use std::time::Duration;
 use tracing::{debug, error, info, trace, warn};
@@ -35,12 +35,17 @@ async fn force_check(server: Server, stream: String) -> Result<(), Error> {
         debug!("{:?}", streams);
         return match streams.into_iter().find(|f| f.id == stream) {
             Some(stream) => match stream.publish_session_info {
-                Some(session) => {
-                    if session.connect_state == RTCPeerConnectionState::Connected {
-                        Ok(())
-                    } else {
-                        Err(anyhow!("connect state is {:?}", session.connect_state))
-                    }
+                Some(_session) => {
+                    Ok(())
+
+                    // NOTE:
+                    // Don't sync do this, because this detect connect state is slow
+                    // parallel is more fast is new connection
+                    //if session.connect_state == RTCPeerConnectionState::Connected {
+                    //    Ok(())
+                    //} else {
+                    //    Err(anyhow!("connect state is {:?}", session.connect_state))
+                    //}
                 }
                 None => Err(anyhow!("Not Found stream publisher")),
             },
