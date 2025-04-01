d9c70dd7ea9cb13fc283dda2cd661eb4d6ddf620
Rocka <i@rocka.me>
2025-04-02 01:19:29 +0800

    fix(web): fix liveman token restore from localStorage

--- web/liveman/liveman.tsx
+++ web/liveman/liveman.tsx
@@ -4,23 +4,26 @@ import { Button } from 'react-daisyui';
 import { useNeedAuthorization } from '@/shared/hooks/use-need-authorization';
 import { StreamsTable } from '@/shared/components/streams-table';
 import { PageLayout } from '@/shared/components/page-layout';
+import * as sharedApi from '@/shared/api';
 
-import * as api from './api';
+import * as livemanApi from './api';
 import { type LoginProps, Login } from './components/login';
 import { NodesTable } from './components/nodes-table';
 import { type IStreamTokenDialog, StreamTokenDialog } from './components/dialog-token';
 
 const TOKEN_KEY = 'liveman_auth_token';
-const initialToken = localStorage.getItem(TOKEN_KEY) ?? '';
-if (initialToken) {
-    api.setAuthToken(initialToken);
+const savedToken = localStorage.getItem(TOKEN_KEY) ?? '';
+const savedTokenValue = savedToken.split(' ')[1] ?? '';
+if (savedToken) {
+    livemanApi.setAuthToken(savedToken);
+    sharedApi.setAuthToken(savedToken);
 }
 
 const initialNodes = new URLSearchParams(location.search).getAll('nodes');
 
 export function Liveman() {
-    const [token, setToken] = useState(initialToken);
-    const [needsAuthorizaiton, setNeedsAuthorization] = useNeedAuthorization(api);
+    const [token, setToken] = useState(savedTokenValue);
+    const [needsAuthorizaiton, setNeedsAuthorization] = useNeedAuthorization(livemanApi);
     const onLoginSuccess: LoginProps['onSuccess'] = (tokenType, tokenValue) => {
         setToken(tokenValue);
         setNeedsAuthorization(false);
@@ -33,7 +36,7 @@ export function Liveman() {
         setFilterNodes(params.getAll('nodes'));
     }, [location.search]);
     const getStreams = useCallback(async () => {
-        const streams = await api.getStreams(filterNodes);
+        const streams = await livemanApi.getStreams(filterNodes);
         return streams.sort((a, b) => a.createdAt - b.createdAt);
     }, filterNodes);
     const getWhxpUrl = (whxp: 'whep' | 'whip', streamId: string) => {
