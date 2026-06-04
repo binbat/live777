0d4b0a93be3f1f934283a4e442f7bcd3527e457a
CK <ck@ckdeMacBook-Pro.local>
2026-06-04 11:48:59 +0800

    refactor: share WHEP playback logic

--- web/player-core/index.ts
+++ web/player-core/index.ts
@@ -2,3 +2,10 @@ export { default as PlayerSurface } from "./player-surface";
 export { default as StatsForNerds } from "./stats";
 export type { StatsNerds } from "./types";
 export { collectWebRtcStats } from "./webrtc-stats";
+export {
+    createWhepPlayback,
+    type WhepMute,
+    type WhepPlayback,
+    type WhepPlaybackOptions,
+    type WhepPlaybackStatus,
+} from "./whep-playback";
